49c28bd35d1c2ac99c184c2dad6bc9e60af98fcd
Erick Hartawan <hartawanerick@gmail.com>
2021-10-06 23:08:38 +1100

    changed folders to lowercase and added Map page

--- src/Home.js
+++ src/Home.js
@@ -1,57 +1,60 @@
-import withRoot from "./modules/withRoot";
+import withRoot from "./modules/withRoot"
 // --- Post bootstrap -----
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ProductCategories from "./modules/views/ProductCategories";
-import ProductSmokingHero from "./modules/views/ProductSmokingHero";
-import AppFooter from "./modules/views/AppFooter";
-import ProductHero from "./modules/views/ProductHero";
-import ProductValues from "./modules/views/ProductValues";
-import ProductHowItWorks from "./modules/views/ProductHowItWorks";
-import ProductCTA from "./modules/views/ProductCTA";
-import AppAppBar from "./modules/views/AppAppBar";
-import Privacy from "./Privacy";
-import Terms from "./Terms";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import ProductCategories from "./modules/views/ProductCategories"
+import ProductSmokingHero from "./modules/views/ProductSmokingHero"
+import AppFooter from "./modules/views/AppFooter"
+import ProductHero from "./modules/views/ProductHero"
+import ProductValues from "./modules/views/ProductValues"
+import ProductHowItWorks from "./modules/views/ProductHowItWorks"
+import ProductCTA from "./modules/views/ProductCTA"
+import AppAppBar from "./modules/views/AppAppBar"
+import Privacy from "./Privacy"
+import Terms from "./Terms"
+import SignIn from "./SignIn"
+import SignUp from "./SignUp"
+import Map from "./Map"
+
+require("dotenv").config()
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Index />
-        </Route>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="/terms">
-          <Terms />
-        </Route>
-        <Route path="/privacy">
-          <Privacy />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Map />
+                </Route>
+                <Route path="/signin">
+                    <SignIn />
+                </Route>
+                <Route path="/signup">
+                    <SignUp />
+                </Route>
+                <Route path="/terms">
+                    <Terms />
+                </Route>
+                <Route path="/privacy">
+                    <Privacy />
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
 
 function Index() {
-  return (
-    <React.Fragment>
-      <AppAppBar />
-      <ProductHero />
-      <ProductValues />
-      <ProductCategories />
-      <ProductHowItWorks />
-      <ProductCTA />
-      <ProductSmokingHero />
-      <AppFooter />
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <AppAppBar />
+            <ProductHero />
+            <ProductValues />
+            <ProductCategories />
+            <ProductHowItWorks />
+            <ProductCTA />
+            <ProductSmokingHero />
+            <AppFooter />
+        </React.Fragment>
+    )
 }
 
-export default withRoot(App);
+export default withRoot(App)
