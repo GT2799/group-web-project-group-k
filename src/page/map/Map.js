e21c85a9ca6d733b58c9c75aac1e2a7b92f5f426
justo414 <justin.lie3@hotmail.com.au>
2021-10-11 16:55:10 +1100

    Add Marker To Map

--- src/page/map/Map.js
+++ src/page/map/Map.js
@@ -1,8 +1,244 @@
-import React from "react"
-import { Map, GoogleApiWrapper } from "google-maps-react"
-import styles from "./Map.module.css"
+import React, {Component} from "react"
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react"
+import "./Map.module.css"
 import { API } from "./API"
 
+const mapStyle = [
+    {
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#1d2c4d"
+        }
+      ]
+    },
+    {
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#8ec3b9"
+        }
+      ]
+    },
+    {
+      elementType: "labels.text.stroke",
+      stylers: [
+        {
+          color: "#1a3646"
+        }
+      ]
+    },
+    {
+      featureType: "administrative.country",
+      elementType: "geometry.stroke",
+      stylers: [
+        {
+          color: "#4b6878"
+        }
+      ]
+    },
+    {
+      featureType: "administrative.land_parcel",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#64779e"
+        }
+      ]
+    },
+    {
+      featureType: "administrative.province",
+      elementType: "geometry.stroke",
+      stylers: [
+        {
+          color: "#4b6878"
+        }
+      ]
+    },
+    {
+      featureType: "landscape.man_made",
+      elementType: "geometry.stroke",
+      stylers: [
+        {
+          color: "#334e87"
+        }
+      ]
+    },
+    {
+      featureType: "landscape.natural",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#023e58"
+        }
+      ]
+    },
+    {
+      featureType: "poi",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#283d6a"
+        }
+      ]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#6f9ba5"
+        }
+      ]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.stroke",
+      stylers: [
+        {
+          color: "#1d2c4d"
+        }
+      ]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry.fill",
+      stylers: [
+        {
+          color: "#023e58"
+        }
+      ]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#3C7680"
+        }
+      ]
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#304a7d"
+        }
+      ]
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#98a5be"
+        }
+      ]
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.stroke",
+      stylers: [
+        {
+          color: "#1d2c4d"
+        }
+      ]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#2c6675"
+        }
+      ]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [
+        {
+          color: "#255763"
+        }
+      ]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#b0d5ce"
+        }
+      ]
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.stroke",
+      stylers: [
+        {
+          color: "#023e58"
+        }
+      ]
+    },
+    {
+      featureType: "transit",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#98a5be"
+        }
+      ]
+    },
+    {
+      featureType: "transit",
+      elementType: "labels.text.stroke",
+      stylers: [
+        {
+          color: "#1d2c4d"
+        }
+      ]
+    },
+    {
+      featureType: "transit.line",
+      elementType: "geometry.fill",
+      stylers: [
+        {
+          color: "#283d6a"
+        }
+      ]
+    },
+    {
+      featureType: "transit.station",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#3a4762"
+        }
+      ]
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: "#0e1626"
+        }
+      ]
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: "#4e6d70"
+        }
+      ]
+    }
+  ];
+  
+
+  
 class MapCont extends React.Component {
     onMapClicked(mapProps, map, clickEvent) {
         console.log("Below is the Lat and Long of the clicked point")
@@ -13,26 +249,38 @@ class MapCont extends React.Component {
         pos.push({ long: lng })
         console.log(pos)
     }
+
+    _mapLoaded(mapProps, map) {
+        map.setOptions({
+          styles: mapStyle
+        });
+      }
+
     render() {
+
+        const containerStyle = {
+            position: "relative",
+            width: "100%",
+            height: "100%"
+          };
+
+
         return (
-            <div className="container">
-                <div className={styles.overlay}>
-                    <div className={styles.overlayInner}>
-                        <h3>hahaha</h3>
-                        <button>hahahah</button>
-                    </div>
-                </div>
-                <div id="map">
-                    <Map
-                        google={this.props.google}
-                        zoom={18}
-                        onClick={this.onMapClicked}
-                        initialCenter={{
-                            lat: -33.7736594370602,
-                            lng: 151.11362303011666,
-                        }}
-                    />
-                </div>
+            <div className="mapContainer">
+                <Map
+                    google={this.props.google}
+                    zoom={18}
+                    style={this.mapStyle}
+                  
+                    onClick={this.onMapClicked}
+                    initialCenter={{
+                        lat: -33.7736594370602,
+                        lng: 151.11362303011666,
+                    }}
+                >
+                <Marker position={this.onMapClicked.pos} />
+                </Map>
+                <h3> Hello </h3>
             </div>
         )
     }
