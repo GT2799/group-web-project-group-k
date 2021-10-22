f3fbb590ccad95ba474f80732979c4426726b3ae
GT2799 <88313016+GT2799@users.noreply.github.com>
2021-10-22 17:06:31 +1100

    is_server_running test

--- api.test.js
+++ api.test.js
@@ -3,3 +3,11 @@ const supertest = require("supertest")
 
 // Initialise server  + router
 const app = require("./Server/app")
+
+
+describe("is_server_running", () =>{
+    it("should be listening", async () =>{
+        await supertest(app).get("/")
+        .expect("404")
+    })
+})
