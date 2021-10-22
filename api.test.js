deeefb148795c74cd2be6ae4d1d09b9fda01d903
GT2799 <88313016+GT2799@users.noreply.github.com>
2021-10-22 17:31:08 +1100

    methods test

--- api.test.js
+++ api.test.js
@@ -9,5 +9,31 @@ describe("is_server_running", () =>{
     it("should be listening", async () =>{
         await supertest(app).get("/")
         .expect(404)
+        .then(res =>{
+            expect(res.headers['x-powered-by']).toBe("Express")
+        })
+    })
+
+    it("api should return a response",async ()=>{
+        await supertest(app).get("/api/")
+            .expect(404)
+            .then(res => {
+                expect(res.headers['content-type']).toBe('text/html; charset=utf-8')
+                expect(res.headers['x-powered-by']).toBe('Express')
+            })
     })
 })
+
+
+// Methods test
+// The only available methods are POST
+
+// GET TEST
+test("GET /", async () =>{
+    await supertest(app).get("/")
+        .expect(404)
+        .then((response) => {
+
+        })
+})
+
